52b472c64f10b91c923befe3944f05cd4a7e4367
Yvette L <yvette.lrox1@gmail.com>
2019-09-14 17:26:06 -0700

    Ugly but works

--- app/app.py
+++ app/app.py
@@ -65,6 +65,33 @@ def logout():
     session['logged_in'] = False
     return hello_world()
 
+from flask_socketio import emit, join_room, leave_room
+
+@socketio.on('joined', namespace='/chat')
+def joined(message):
+    """Sent by clients when they enter a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': session.get('firstname') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    room = session.get('room')
+    emit('message', {'msg': session.get('firstname') + ':' + message['msg']}, room=room)
+
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    """Sent by clients when they leave a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    leave_room(room)
+    emit('status', {'msg': session.get('firstname') + ' has left the room.'}, room=room)
+
 
 @app.route('/chat')
 def chat():
